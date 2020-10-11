5b70fd2d6e1f1c49dff14975c2b3d38e3a308d10
FMS0102 <felsimoes@gmail.com>
2020-10-10 23:34:54 -0300

    User entity and resource

---
+++ src/main/java/com/fms/webservice/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.fms.webservice.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fms.webservice.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Felipe", "felsimoes@gmail.com", "88888888", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
