a1f068187c4c3fd5a91cf35bbdcdbe136f01f221
FMS0102 <felsimoes@gmail.com>
2021-02-07 14:50:21 -0300

    Payment, one-to-one association

--- src/main/java/com/fms/webservice/config/TestConfig.java
+++ src/main/java/com/fms/webservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.fms.webservice.entities.Category;
 import com.fms.webservice.entities.Order;
 import com.fms.webservice.entities.OrderItem;
+import com.fms.webservice.entities.Payment;
 import com.fms.webservice.entities.Product;
 import com.fms.webservice.entities.User;
 import com.fms.webservice.entities.enums.OrderStatus;
@@ -28,7 +29,7 @@ public class TestConfig implements CommandLineRunner {
 	private UserRepository userRepository;
 
 	@Autowired
-	private OrderRepository orderRepoesitory;
+	private OrderRepository orderRepository;
 
 	@Autowired
 	private CategoryRepository categoryRepository;
@@ -72,7 +73,7 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
-		orderRepoesitory.saveAll(Arrays.asList(o1, o2, o3));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 
 }
